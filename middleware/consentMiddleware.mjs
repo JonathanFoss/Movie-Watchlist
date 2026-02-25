4e95b521c318414cff6e61d645dce6df84d98ad3
JonathanFoss <jonathanfoss01@gmail.com>
2026-02-25 13:31:13 +0100

    changed export
    Changed export default for bug in render. Incase this fixes it.

--- middleware/consentMiddleware.mjs
+++ middleware/consentMiddleware.mjs
@@ -1,7 +1,9 @@
-export default function requireConsent(req, res, next) {
+function requireConsent(req, res, next) {
   const { consent } = req.body;
   if (!consent || !consent.tos || !consent.privacy) {
     return res.status(400).json({ message: "User must accept ToS and Privacy Policy" });
   }
   next();
 }
+
+export default requireConsent;
